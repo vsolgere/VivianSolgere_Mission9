5aa40fac38f33d92d0cbf8d389339dd9a9086810
vsolgere <viviansolgere@gmail.com>
2024-03-08 23:09:50 -0600

    Basketball teams!!

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import './App.css';
-import logo from './logo.svg';
+import React from "react";
+import "./App.css";
+import teamData from "./CollegeBasketballTeams.json";
 
 function App() {
+  function Welcome() {
+    return <h1>2024 March Madness Teams</h1>;
+  }
+
+  interface TeamProps {
+    school: string;
+    mascot: string;
+    location: string;
+  }
+
+  function TeamList() {
+    return (
+      <div>
+        {teamData.teams.map((team) => (
+          <Team
+            key={team.tid}
+            school={team.school}
+            mascot={team.name}
+            location={`${team.city}, ${team.state}`}
+          />
+        ))}
+      </div>
+    );
+  }
+
+  class Team extends React.Component<TeamProps> {
+    render() {
+      const { school, mascot, location } = this.props;
+      return (
+        <div>
+          <h2>{school}</h2>
+          <p>Mascot: {mascot}</p>
+          <p>Location: {location}</p>
+        </div>
+      );
+    }
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome />
+      <TeamList />
     </div>
   );
 }
